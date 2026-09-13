026a8e49bc1b8b5e382ee370ab63ab9f2d139f8d
Harshil Khandelwal <161931283+Harshilkh7@users.noreply.github.com>
2026-09-13 16:51:16 +0530

    feat: mount realtime notification UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import RealtimeBridge from "./components/RealtimeBridge";
+import RealtimeToast from "./components/RealtimeToast";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RealtimeBridge />
+    <RealtimeToast />
     <App />
   </React.StrictMode>
 );
