aa96a0b103a9e8751653a97797720b3417b441fa
Harshil Khandelwal <161931283+Harshilkh7@users.noreply.github.com>
2026-09-13 16:31:29 +0530

    feat: add subscription route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AdminDashboard from "./pages/AdminDashboard";
@@ -15,103 +10,25 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import StudentDashboard from "./pages/StudentDashboard";
 import MyLeaves from "./pages/MyLeaves";
 import MyComplaints from "./pages/MyComplaints";
+import Subscription from "./pages/Subscription";
+
+const Protected = ({ children }) => <ProtectedRoute>{children}</ProtectedRoute>;
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-        <Route
-          path="/hostels"
-          element={
-            <ProtectedRoute>
-              <Hostels />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/rooms"
-          element={
-            <ProtectedRoute>
-              <Rooms />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/rooms"
-          element={
-            <ProtectedRoute>
-              <Rooms />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route 
-          path="/leaves" 
-          element={
-            <ProtectedRoute>
-              <Leaves />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/complaints" 
-          element={
-            <ProtectedRoute>
-              <Complaints />
-            </ProtectedRoute>
-          } 
-        />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/student"
-          element={
-            <ProtectedRoute>
-              <StudentDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/my-leaves"
-          element={
-            <ProtectedRoute>
-              <MyLeaves />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/my-complaints"
-          element={
-            <ProtectedRoute>
-              <MyComplaints />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/hostels" element={<Protected><Hostels /></Protected>} />
+        <Route path="/rooms" element={<Protected><Rooms /></Protected>} />
+        <Route path="/leaves" element={<Protected><Leaves /></Protected>} />
+        <Route path="/complaints" element={<Protected><Complaints /></Protected>} />
+        <Route path="/admin" element={<Protected><AdminDashboard /></Protected>} />
+        <Route path="/student" element={<Protected><StudentDashboard /></Protected>} />
+        <Route path="/my-leaves" element={<Protected><MyLeaves /></Protected>} />
+        <Route path="/my-complaints" element={<Protected><MyComplaints /></Protected>} />
+        <Route path="/subscription" element={<Protected><Subscription /></Protected>} />
       </Routes>
     </BrowserRouter>
   );
