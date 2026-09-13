ea62e104b3ae63327726b6de565f669c336520ea
Harshil Khandelwal <161931283+Harshilkh7@users.noreply.github.com>
2026-09-13 16:31:04 +0530

    feat: mount realtime bridge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
-
+import RealtimeBridge from "./components/RealtimeBridge";
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <RealtimeBridge />
     <App />
   </React.StrictMode>
 );
